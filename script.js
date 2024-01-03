3246016c4742bc5d335dcd6c10376c2ce456ea7a
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2024-01-03 11:48:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Your code here.
+let isMouseDown = false;
+let startX, scrollLeft;
+let items=document.querySelector(".items");
+items.addEventListener("mousedown",function(){
+	isMouseDown = true;
+  startX = e.pageX - items.offsetLeft;
+  scrollLeft = items.scrollLeft;
+}) ;
+function endDrag() {
+  isMouseDown = false;
+}
+function drag(e) {
+  if (!isMouseDown) return;
+  e.preventDefault();
+
+  const x = e.pageX - items.offsetLeft;
+  const walk = (x - startX) * 2;
+ items.scrollLeft = scrollLeft - walk;
+}
+document.addEventListener('mouseup', endDrag);
+document.addEventListener('mousemove', drag);
